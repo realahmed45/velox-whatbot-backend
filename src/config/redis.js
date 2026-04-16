550d5df24d8fc2fcf62e7cfd2f030c59d909df9a
realahmedali4@gmail.com <realahmedali4@gmail.com>
2026-04-16 20:26:09 +0500

    new commit 7

--- src/config/redis.js
+++ src/config/redis.js
@@ -5,14 +5,14 @@ let redisClient;
 
 const getRedisClient = () => {
   if (!redisClient) {
-    redisClient = new Redis(
-      process.env.REDIS_URL || "redis://localhost:6379",
-      {
-        maxRetriesPerRequest: null,
-        tls: process.env.REDIS_URL && process.env.REDIS_URL.startsWith("rediss://") ? {} : undefined,
-        retryStrategy: (times) => Math.min(times * 50, 2000),
-      },
-    );
+    redisClient = new Redis(process.env.REDIS_URL || "redis://localhost:6379", {
+      maxRetriesPerRequest: null,
+      tls:
+        process.env.REDIS_URL && process.env.REDIS_URL.startsWith("rediss://")
+          ? {}
+          : undefined,
+      retryStrategy: (times) => Math.min(times * 50, 2000),
+    });
     redisClient.on("connect", () => logger.info("Redis connected"));
     redisClient.on("error", (err) =>
       logger.error(`Redis error: ${err.message}`),
